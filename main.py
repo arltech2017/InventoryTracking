8e788e5f2eec266e6ed4229ef83f3aa7a0292f9a
Ellis Sutko <esutko@gmail.com>
2017-09-21 12:26:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def save_catalog(catalog, fname):
-    """Write catalog to json encoded fname file."""
-    f = open(fname, 'w')
-    catalog = [item.__dict__ for item in catalog]
-    f.write(json.dumps(catalog))
-    f.close()
+import sys
+from lib import *
 
+if (len(sys.argv) > 1) and (sys.argv[1] == 'test'):
+        import os 
+        import doctest
+        tests = os.listdir("tests/")
+        for file in tests:
+            doctest.testfile("tests/{}".format(file))
+        exit(0)
